5f58b80f7c8061982141f1061e196f8d75692d16
ACG <acthegreat133@gmail.com>
2017-10-27 22:08:34 -0700

    Comments on TeleopTest.Java
    Should make this easier to read and understand

--- FTC8564_2017_2018/TeamCode/src/main/java/ftc8564opMode/TeleopTest.java
+++ FTC8564_2017_2018/TeamCode/src/main/java/ftc8564opMode/TeleopTest.java
@@ -18,33 +18,57 @@ import static java.lang.Math.abs;
 /*@Disabled*/
 
 public class TeleopTest extends OpMode{
+    /**
+    * TeleopTest.java is created by A.C.G. for team LockDown #8564.
+    * (c) Copyright 20xx CA Works. All Rights reserved.
+    *
+    * This is a debug teleop program which introduces the basic movement of the robot.
+    * Later the definitions will be moved to a hardware class.
+    *
+    */
+
 
     private ElapsedTime runtime = new ElapsedTime();
+
+    /*object creation (will move to a hardware library)*/
+    /*Drive Wheels (Motors)*/
     DcMotor motorleft;
     DcMotor motorright;
-
+    /*clamp motor*/
     Servo clampleft;
     Servo clampright;
-
+    /*lift motors*/
     DcMotor liftleft;
     DcMotor liftright;
-
+    /*intake motors*/
     DcMotor intakeleft;
     DcMotor intakeright;
+    /*
+    * Special values definition:
+    * reverseFactor: multiplying with this to reverse the directon of the motor
+    * threshold: this is the common threshold for regular uses.
+    * Warning: while using threshold, there must be a reset statement to set it to 0
+    *
+    */
+    private int reverseFactor = -1;
+    private double threshold = 0.1;
+
+
     @Override
     public void init() {
-        telemetry.addData("Status", "Initialized");
 
+        telemetry.addData("Status", "Initialized");
+        /* hardware mapping */
+        /*Drive Wheels (Motors)*/
         motorleft = hardwareMap.dcMotor.get("left");
         motorright = hardwareMap.dcMotor.get("right");
-
-
+        /*clamp motor*/
         clampleft = hardwareMap.servo.get("clampleft");
         clampright = hardwareMap.servo.get("clampright");
-
+        /*lift motors*/
         liftleft = hardwareMap.dcMotor.get("liftleft");
         liftright = hardwareMap.dcMotor.get("liftright");
-
+        /*intake motors*/
         intakeleft = hardwareMap.dcMotor.get("intakeleft");
         intakeright = hardwareMap.dcMotor.get("intakeright");
 
@@ -58,6 +82,7 @@ public class TeleopTest extends OpMode{
 
     @Override
     public void init_loop() {
+        /*sets servo position */
         clampleft.setPosition(0);
         /*clampright.setPosition(90);*/
 
@@ -70,7 +95,7 @@ public class TeleopTest extends OpMode{
     @Override
     public void start() {
 
-        clampleft.setPosition(30);
+        clampleft.setPosition(20);
         /*clampright.setPosition(75);*/
     }
 
@@ -83,7 +108,16 @@ public class TeleopTest extends OpMode{
      */
     @Override
     public void loop() {
+
+        /*
+         * main code body
+         * These codes will be ran in a loop continuously.
+         * Main actions are in here, as well as updates.
+         *
+         */
+
         runtime.reset();
+
         /*
         if(gamepad1.left_stick_y >0.1){
             motorleft.setPower(gamepad1.left_stick_y);
@@ -98,14 +132,25 @@ public class TeleopTest extends OpMode{
             motorright.setPower(gamepad1.left_stick_y);
         }
         */
-
-        if (abs(gamepad1.left_stick_y) > 0.1){
-            motorleft.setPower(gamepad1.left_stick_y *-1);
+        /* Driving
+        *  The previously commented out ones are the implementation of single joystick drive.
+        *  This implementation below is tank drive.
+        *
+        *  absolute values are used to measure threshold
+        *  while the main value assignment is not modified.
+        *  In here, all LEFT motors will be REVERSED
+        *
+        *  Player 1 controls: left/right joystick up/down ->
+        *  corresponding side wheels move forward/backward
+        *
+        */
+        if (abs(gamepad1.left_stick_y) > threshold){
+            motorleft.setPower(gamepad1.left_stick_y *reverseFactor);
         }
         else{
                 motorleft.setPower(0 );
         }
-        if (abs(gamepad1.right_stick_y) > 0.1){
+        if (abs(gamepad1.right_stick_y) > threshold){
             motorright.setPower(gamepad1.right_stick_y );
         }
         else {
@@ -113,9 +158,18 @@ public class TeleopTest extends OpMode{
         }
 
 
-
-        if (abs(gamepad2.right_stick_y) > 0.1){
-            liftleft.setPower(gamepad2.right_stick_y * -1);
+        /* Lifting
+        *  The lift is done by moving two motors simultaniously.
+        *  Additional movements (in case one motor overspun, etc will be added soon.
+        *
+        *  WARNING: fragile lift.
+        *  Clamp system coming soon.
+        *
+        * Player 2 controls: Right Joystick up/down -> lift up/down
+        *
+        */
+        if (abs(gamepad2.right_stick_y) > threshold){
+            liftleft.setPower(gamepad2.right_stick_y * reverseFactor);
             liftright.setPower(gamepad2.right_stick_y);
         }
         else{
@@ -123,10 +177,19 @@ public class TeleopTest extends OpMode{
             liftright.setPower(0);
         }
 
+        /*
+        * Intake - Intake portion.
+        *
+        * if both left and right bumper are both pushed above threshold -> both sides intake at 100%
+        * if only left/right pushed -> corresponding side goes 100%, while the other side lowers
+        * if nothing pushed -> stopped
+        *
+        * Player 1 controls: left trigger and Right Trigger
+        */
 
         if (abs(gamepad1.left_trigger) > 0.6 && abs(gamepad1.right_trigger) > 0.6){
             intakeleft.setPower(1 );
-            intakeright.setPower(1*-1);
+            intakeright.setPower(1 * reverseFactor);
         }
         else if (abs(gamepad1.left_trigger) > 0.6) {
             intakeleft.setPower(1);
@@ -134,25 +197,41 @@ public class TeleopTest extends OpMode{
         }
         else if (abs(gamepad1.right_trigger) > 0.6) {
             intakeleft.setPower(0.6);
-            intakeright.setPower(1 * -1);
+            intakeright.setPower(1 * reverseFactor);
         }
         else {
             intakeleft.setPower(0);
-            intakeright.setPower(0 * -1);
+            intakeright.setPower(0 * reverseFactor);
         }
 
+        /*
+        * Intake -> slant cube adjustment
+        * By using bumpers, the corresponded bumper side will go IN while the other side will go OUT
+        *
+        * Player 1 Control: Left bumper/Right Bumper: corresponding side rolls in while other goes
+        * reverse
+        */
         if (gamepad1.left_bumper){
             intakeleft.setPower(1);
-            intakeright.setPower(-1*-1);
+            intakeright.setPower(-1*reverseFactor);
+        }
+        else {
+            intakeleft.setPower(0);
+            intakeright.setPower(0 * reverseFactor);
         }
 
         if (gamepad1.right_bumper){
             intakeleft.setPower(-1);
-            intakeright.setPower(1*-1);
+            intakeright.setPower(1 * reverseFactor);
+        }
+        else {
+            intakeleft.setPower(0);
+            intakeright.setPower(0 * reverseFactor);
         }
 
 
 
         telemetry.addData("Status", "Run Time: " + runtime.toString());
+
     }
 }
