742c5f48627c1eab39c63a21019cdf1824d3ec95
Margaret Li <margaretli2101@gmail.com>
2017-12-29 20:27:35 -0800

    imported opencv

---
+++ FtcRobotController/src/main/java/ftc/vision/FrameGrabber.java
@@ -0,0 +1,33 @@
+package ftc.vision;
+
+import android.view.SurfaceView;
+
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.core.Mat;
+
+/**
+ * Created by margaretli on 12/29/17.
+ */
+
+public class FrameGrabber implements
+        CameraBridgeViewBase.CvCameraViewListener2 {
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return inputFrame.rgba();
+    }
+
+    public FrameGrabber(CameraBridgeViewBase c) {
+        c.setVisibility(SurfaceView.VISIBLE);
+        c.setCvCameraViewListener(this);
+    }
+}
